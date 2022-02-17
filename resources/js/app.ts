e9b6c2972aacbca2b6ef470c1e1a9480d33e92f0
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-17 17:34:31 +0200

    enabled vue-toastification

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,7 +2,7 @@ import '../css/app.css'
 import './bootstrap.ts'
 
 import { App, plugin as inertiaPlugin } from '@inertiajs/inertia-vue3'
-// import Toast, { POSITION } from 'vue-toastification'
+import Toast, { POSITION } from 'vue-toastification'
 import { createApp, h } from 'vue'
 
 import { InertiaProgress } from '@inertiajs/progress'
@@ -10,29 +10,29 @@ import { InertiaProgress } from '@inertiajs/progress'
 const el = document.getElementById('app')!
 
 let asyncViews = () => {
-    return import.meta.glob('./Pages/**/*.vue')
+	return import.meta.glob('./Pages/**/*.vue')
 }
 
 createApp({
-    render: () =>
-        h(App, {
-            initialPage: JSON.parse(el.dataset.page!),
-            resolveComponent: async (name: string) => {
-                if (import.meta.env.DEV) {
-                    // console.log(import.meta.env)
-                    return (await import(`./Pages/${name}.vue`)).default
-                } else {
-                    let pages = asyncViews()
-                    const importPage = pages[`./Pages/${name}.vue`]
-                    return importPage().then((module) => module.default)
-                }
-            },
-        }),
+	render: () =>
+		h(App, {
+			initialPage: JSON.parse(el.dataset.page!),
+			resolveComponent: async (name: string) => {
+				if (import.meta.env.DEV) {
+					// console.log(import.meta.env)
+					return (await import(`./Pages/${name}.vue`)).default
+				} else {
+					let pages = asyncViews()
+					const importPage = pages[`./Pages/${name}.vue`]
+					return importPage().then((module) => module.default)
+				}
+			},
+		}),
 })
-    .use(inertiaPlugin)
-    // .use(Toast, {
-    //     position: POSITION.TOP_CENTER,
-    // })
-    .mount(el)
+	.use(inertiaPlugin)
+	.use(Toast, {
+		position: POSITION.TOP_CENTER,
+	})
+	.mount(el)
 
 InertiaProgress.init({ color: '#4B5563' })
