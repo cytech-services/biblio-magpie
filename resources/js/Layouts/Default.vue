2b20f34b829ce8fb89b904b791de038d04bd4df0
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-17 17:36:18 +0200

    putting together default app scaffolding

---
+++ resources/js/Layouts/Default.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="min-h-full">
+		<navigation :user="user" />
+
+		<div>
+			<Head :title="title" />
+
+			<header class="bg-white shadow-sm">
+				<div class="w-full mx-auto py-4 px-4 sm:px-6 lg:px-8">
+					<h1 class="text-lg leading-6 font-semibold text-gray-900">{{ title }}</h1>
+				</div>
+			</header>
+			<main>
+				<slot />
+			</main>
+		</div>
+	</div>
+</template>
+
+<script>
+import Navigation from '@/Components/Navigation.vue'
+
+const user = {
+	name: 'Tom Cook',
+	email: 'tom@example.com',
+	imageUrl:
+		'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+
+export default {
+	components: {
+		Navigation,
+	},
+	props: {
+		title: {
+			type: String,
+			default: 'Test',
+		},
+	},
+	setup() {
+		return {
+			user,
+		}
+	},
+}
+</script>
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-        <h1>Welcome</h1>
-    </div>
+	<default-layout title="Dashboard">
+		<div class="w-full mx-auto py-6 sm:px-6 lg:px-8">
+			<!-- Replace with your content -->
+			<div class="px-4 py-4 sm:px-0">
+				<div class="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+			</div>
+			<!-- /End replace -->
+		</div>
+	</default-layout>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import DefaultLayout from '@/Layouts/Default.vue'
 
-export default defineComponent({
-    setup() {
-        console.log('Page - Index')
-    },
-})
+export default {
+	components: {
+		DefaultLayout,
+	},
+	props: {
+		// user: Object,
+	},
+}
 </script>
