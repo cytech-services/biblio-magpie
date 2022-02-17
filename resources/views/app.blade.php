d3cecccb241c47a388a2bf460f5403a8acc19726
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-17 17:35:37 +0200

    fixed page styling and css loading for production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,13 +11,12 @@
                 $manifest = json_decode(file_get_contents(public_path('dist/manifest.json')), true);
             @endphp
             <script type="module" src="/dist/{{ $manifest['resources/js/app.ts']['file'] }}"></script>
-            {{-- <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.ts']['css'][0] }}"> --}}
+            <link rel="stylesheet" href="/dist/{{ $manifest['resources/js/app.ts']['css'][0] }}">
         @else
-            {{-- <script type="module" src="http://localhost:3030/@vite/client"></script> --}}
             <script type="module" src="http://localhost:3030/resources/js/app.ts"></script>
         @endproduction
     </head>
-    <body>
+    <body class="h-full">
         @inertia
     </body>
 </html>
