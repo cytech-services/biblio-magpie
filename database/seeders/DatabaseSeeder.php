f17d8e5251ea627fb901a8dfe5f33bbff47c4cd7
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-22 09:07:56 +0200

    moved seeding logic from DatabaseSeeder to LibrarySeeder and disabled autoseeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,15 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Author::factory(30)->create();
-        \App\Models\Category::factory(15)->create();
-        \App\Models\FileFormat::factory(3)->create();
-        \App\Models\IdentificationType::factory(20)->create();
-        \App\Models\Publisher::factory(20)->create();
-        \App\Models\Series::factory(10)->create();
-
         $this->call([
-            LibrarySeeder::class,
+            // LibrarySeeder::class,
         ]);
     }
 }
--- database/seeders/LibrarySeeder.php
+++ database/seeders/LibrarySeeder.php
@@ -14,6 +14,13 @@ class LibrarySeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\Author::factory(30)->create();
+        \App\Models\Category::factory(15)->create();
+        \App\Models\FileFormat::factory(3)->create();
+        \App\Models\IdentificationType::factory(20)->create();
+        \App\Models\Publisher::factory(20)->create();
+        \App\Models\Series::factory(10)->create();
+
         Library::factory(2)
             ->hasBooks(500)
             ->create();
