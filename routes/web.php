802251398fabd7c65127ec5a8fb18da9a1278326
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-26 11:01:19 +0200

    redirect root to library

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,16 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::redirect('/', '/library');
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -21,9 +23,6 @@ Route::get('/dashboard', function () {
 require __DIR__ . '/auth.php';
 
 Route::resource('library', LibraryController::class)->middleware(['auth', 'verified']);
-
-
-// Route::redirect('/', '/library');
 
 // Route::controller(LibraryController::class)->group(function () {
 //     Route::get('/library', 'index')->name('library.index');
