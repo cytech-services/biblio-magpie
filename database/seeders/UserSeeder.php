6f9b9a585ab4bd8749954c6ea27698e5d423eb29
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-27 17:58:29 +0200

    - added default users to UserSeeder for testing - added UserSeeder to DatabaseSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +15,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(5)->create();
+        $user = User::create([
+            'name' => 'Christopher Graham',
+            'email' => 'chris@cytech.services',
+            'password' => Hash::make('uCfTHiHamRvy'),
+        ]);
+        $user->assignRole('Super Admin');
+
+        $user = User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@cytech.services',
+            'password' => Hash::make('uCfTHiHamRvy'),
+        ]);
+        $user->assignRole('Admin');
+
+        $user = User::create([
+            'name' => 'Librarian User',
+            'email' => 'librarian@cytech.services',
+            'password' => Hash::make('uCfTHiHamRvy'),
+        ]);
+        $user->assignRole('Librarian');
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@cytech.services',
+            'password' => Hash::make('uCfTHiHamRvy'),
+        ]);
+        $user->assignRole('User');
     }
 }
