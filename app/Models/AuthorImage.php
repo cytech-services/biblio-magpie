1aa9dd4c3c08501baa56b98d19d538d3aada5da9
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-02-27 23:44:37 +0200

    added model with fillable, casts and relations

--- app/Models/AuthorImage.php
+++ app/Models/AuthorImage.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class AuthorImage extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'author_id',
+        'format',
+        'path',
+        'size',
+    ];
+
+    protected $appends = ['url'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'id' => 'integer',
+        'size' => 'integer',
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function getUrlAttribute()
+    {
+        return Storage::disk('books')->url($this->path);
+    }
 }
