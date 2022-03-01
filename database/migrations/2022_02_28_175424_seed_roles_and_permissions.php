10ef2ac84d2ad6d29862e578443237af4df09cf8
Christopher Graham <69510509+cytech-services@users.noreply.github.com>
2022-03-01 09:05:07 +0200

    moved roles and permissions seeder to a migration

---
+++ database/migrations/2022_02_28_175424_seed_roles_and_permissions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Call seeder
+        Artisan::call('db:seed', [
+            '--class' => 'RolesAndPermissionsSeeder',
+            '--force' => true
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            RolesAndPermissionsSeeder::class,
             UserSeeder::class,
             // LibrarySeeder::class,
         ]);
